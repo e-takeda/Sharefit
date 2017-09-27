a0fc735b920469b9cdfacbfc597a5e277a634b33
nttr <nttr@BI-23556.local>
2017-09-27 16:57:13 +0900

    ユーザー画像アップロード途中

---
+++ Sharefit/BorderButton.swift
@@ -0,0 +1,33 @@
+//
+//  BorderButton.swift
+//  Sharefit
+//
+//  Created by nttr on 2017/09/27.
+//  Copyright © 2017年 nttr. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {    
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
--- Sharefit/EditUserInfoViewController.swift
+++ Sharefit/EditUserInfoViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import NCMB
 
-class EditUserInfoViewController: UIViewController ,UITextFieldDelegate ,UITextViewDelegate{
+class EditUserInfoViewController: UIViewController ,UITextFieldDelegate ,UITextViewDelegate ,UIImagePickerControllerDelegate ,UINavigationControllerDelegate{
     
     @IBOutlet var userImageView: UIImageView!
     @IBOutlet var userNameTextField: UITextField!
@@ -49,6 +49,71 @@ class EditUserInfoViewController: UIViewController ,UITextFieldDelegate ,UITextV
         textView.resignFirstResponder()
         return true
     }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let selectedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        let resizedImage = selectedImage.scale(byFactor: 0.4)
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        let data = UIImagePNGRepresentation(resizedImage!)
+        let file = NCMBFile.file(withName: NCMBUser.current().objectId, data: data) as! NCMBFile
+        file.saveInBackground({ (error) in
+            if error != nil {
+                let alert = UIAlertController(title: "画像アップロードエラー", message: error!.localizedDescription, preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default, handler: { (action) in
+                    
+                })
+                alert.addAction(okAction)
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                self.userImageView.image = selectedImage
+            }
+        }) { (progress) in
+            print(progress)
+        }
+    }
+
+    
+    
+    
+    @IBAction func selectImage() {
+        let actionController = UIAlertController(title: "画像の選択", message: "選択して下さい", preferredStyle: .actionSheet)
+        let cameraAction = UIAlertAction(title: "カメラ", style: .default) { (action) in
+            // カメラ起動
+            if UIImagePickerController.isSourceTypeAvailable(.camera) == true {
+                let picker = UIImagePickerController()
+                picker.sourceType = .camera
+                picker.allowsEditing = true
+                picker.delegate = self
+                self.present(picker, animated: true, completion: nil)
+            } else {
+                print("この機種ではカメラが使用出来ません。")
+            }
+        }
+        let albumAction = UIAlertAction(title: "フォトライブラリ", style: .default) { (action) in
+            // アルバム起動
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) == true {
+                let picker = UIImagePickerController()
+                picker.sourceType = .photoLibrary
+                picker.allowsEditing = true
+                picker.delegate = self
+                self.present(picker, animated: true, completion: nil)
+            } else {
+                print("この機種ではフォトライブラリが使用出来ません。")
+            }
+        }
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel) { (action) in
+            actionController.dismiss(animated: true, completion: nil)
+        }
+        actionController.addAction(cameraAction)
+        actionController.addAction(albumAction)
+        actionController.addAction(cancelAction)
+        self.present(actionController, animated: true, completion: nil)
+    }
+
     
     @IBAction func closeEditViewController() {
         self.dismiss(animated: true, completion: nil)
